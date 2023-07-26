963b7c28a826277b028d08eec55e81fc790682f2
Your Name <you@example.com>
2023-07-26 09:03:13 +0800

    17

---
+++ week3/0717/Aa.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>標籤連結</title>
+    <style>
+
+    </style>
+</head>
+<body>
+    <div class = "D">
+        <p>Google</p>
+        <a href="https://www.google.com"></a><br>
+    </div>
+    <div class = "D">
+        <p>Yoptube</p>
+        <a href="https://www.youtube.com"></a>
+    </div>
+    <div class = "D">
+        <p>ChatGPT</p>
+        <a href="https://chat.openai.com"></a>
+    </div>
+</body>
+</html>
